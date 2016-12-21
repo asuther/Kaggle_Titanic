a25cc8b26162f00014811dc8eb42e12c7fb9c7a2
asuther <alexander.m.sutherland@gmail.com>
2016-12-20 21:48:15 -0800

    Started exploring data and performing feature engineering

---
+++ src/Data_Loader.py
@@ -0,0 +1,26 @@
+import os
+
+import numpy as np
+import pandas as pd
+
+package_directory = os.path.dirname(os.path.abspath(__file__))
+
+def load_training_data():
+	"""
+
+	Returns X and y for the Titanic training dataset
+
+	"""
+	#Load the raw data
+	data_train = pd.read_csv(package_directory + '/../data/raw/train.csv')
+
+	#Index the dataset according to the PassengerId
+	data_train_indexed = data_train.set_index('PassengerId')
+	
+	#Create the design matrix and target vector
+	X = data_train_indexed.drop(['Survived'], axis=1)
+	y = data_train_indexed['Survived']
+
+	return X, y
+
+	
