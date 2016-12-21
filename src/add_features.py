ab2a2cbb06a0436ee423c03d19855c8bfc0c2231
asuther <alexander.m.sutherland@gmail.com>
2016-12-21 07:46:18 -0800

    Moved loading features to general functions

---
+++ src/add_features.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+def load_training_data(data_path = 'raw/train.csv'):
+	"""
+
+	Returns X and y for the Titanic training dataset
+
+	"""
+	#Load the raw data
+	data_train = pd.read_csv(package_directory + '/../data/%s' % data_path)
+
+	#Index the dataset according to the PassengerId
+	data_train_indexed = data_train.set_index('PassengerId')
+	
+	#Create the design matrix and target vector
+	X = data_train_indexed.drop(['Survived'], axis=1)
+	y = data_train_indexed['Survived']
+
+	return X, y
+
+	
